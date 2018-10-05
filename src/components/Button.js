c1028dcff5e0a5c32d04943fa0664542257e39f4
Hiromitsu Yamaguchi <yamagame@mac.com>
2018-10-05 12:56:29 +0900

    微修正

--- src/components/Button.js
+++ src/components/Button.js
@@ -68,7 +68,6 @@ class Button extends Component {
         <div style={{ height: '100%' }}>
           <div className={['Button-Key', fade ? 'Button-Key-Fade' : '', this.props.correct ? 'Button-Key-Correct' : ''].join(' ')}
             ref={ bt => this.button = bt }
-            type="button"
             style={ {
               fontSize: `${parseInt(fontSize*scale, 10)}px`,
               backgroundColor: this.backgroundColor(),
