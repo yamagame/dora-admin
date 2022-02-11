8f102be70e66527ca709ab519da977220cac0a7d
yamagame <yamagame@mac.com>
2022-02-11 19:23:25 +0900

    メンテナンス

--- copy-to-dora.sh
+++ copy-to-dora.sh
@@ -4,5 +4,5 @@ rm ../dora-engine/public/admin-page/index.html
 rm ../dora-engine/public/static/js/main-admin.*.js
 rm ../dora-engine/public/static/css/main-admin.*.css
 cp ./build/index.html ../dora-engine/public/admin-page/
-cp ./build/static/js/main-*.js ../dora-engine/public/static/js/
-cp ./build/static/css/main-*.css ../dora-engine/public/static/css/
+cp ./build/static/js/main.*.js ../dora-engine/public/static/js/
+cp ./build/static/css/main.*.css ../dora-engine/public/static/css/
